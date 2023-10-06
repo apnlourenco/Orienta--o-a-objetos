22c3987f1408b984a060cb5ba842a777f41bcdf1
Ana Paula Lourenço <135036574+apnlourenco@users.noreply.github.com>
2023-10-05 23:12:36 -0300

    Update Sorteio.java

--- Sorteio.java
+++ Sorteio.java
@@ -1,3 +1,29 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Sorteio {
-    
+
+      private ArrayList<Integer> numeros;
+
+    public Sorteio() {
+        numeros = new ArrayList<>();
+    }
+
+    public boolean sortear() {
+        Random random = new Random();
+
+        for (int i = 0; i < 6; i++) {
+            int numero = random.nextInt(60) + 1;
+
+            while (numeros.contains(numero)) {
+                numero = random.nextInt(60) + 1;
+            }
+
+            numeros.add(numero);
+        }
+    }
+
+    public Integer[] getNumeros() {
+        return numeros.toArray(new Integer[0]);
+    }
 }
